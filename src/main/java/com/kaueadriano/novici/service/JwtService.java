25052ffc08b738dff88f8ac2a4e6f1f44d088731
Kauê Adriano <166442178+kaue0000@users.noreply.github.com>
2026-09-23 16:47:09 -0300

    feat: Adiciona módulo de autenticação

---
+++ src/main/java/com/kaueadriano/novici/service/JwtService.java
@@ -0,0 +1,43 @@
+package com.kaueadriano.novici.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final SecretKey secretKey = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
+
+    private final long TIMETOEXPIRE = 1000 * 60 * 60 * 10; // 10 Horas em milissegundos
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + TIMETOEXPIRE))
+                .signWith(secretKey)
+                .compact();
+    }
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+    public Boolean isTokenValid(String token){
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
