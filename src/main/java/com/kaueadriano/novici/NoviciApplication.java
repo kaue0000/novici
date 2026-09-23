3093bd296ae28f73583894be0416416beb1be2ce
Kauê Adriano <166442178+kaue0000@users.noreply.github.com>
2026-09-23 16:42:25 -0300

    feat: Adiciona estrutura inicial

---
+++ src/main/java/com/kaueadriano/novici/NoviciApplication.java
@@ -0,0 +1,13 @@
+package com.kaueadriano.novici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoviciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaueadriano/novici/NoviciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaueadriano.novici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoviciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
