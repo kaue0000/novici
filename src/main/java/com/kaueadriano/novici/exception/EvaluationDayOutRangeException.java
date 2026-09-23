690f1ad0efa387c6084d59496d4513bcd7117428
Kauê Adriano <166442178+kaue0000@users.noreply.github.com>
2026-09-23 19:18:35 -0300

    feat: Adiciona módulo de Login

---
+++ src/main/java/com/kaueadriano/novici/exception/EvaluationDayOutRangeException.java
@@ -0,0 +1,7 @@
+package com.kaueadriano.novici.exception;
+
+public class EvaluationDayOutRangeException extends RuntimeException {
+    public EvaluationDayOutRangeException(String message) {
+        super(message);
+    }
+}
